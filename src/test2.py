5ab74f16a42d0633820ba9e361eb8dd193c740a1
Hrishikesh Saraiya <U720149@na.o-i.intra>
2019-05-30 09:48:05 -0400

    tweets scraped and saved in csv

---
+++ src/test2.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import tweepy
+import csv
+
+auth = tweepy.auth.OAuthHandler('DSES0LTFK2vAFV3zGAwQlJXDa', 'KLVUqauEqqelnZZPry9GPTvHuLrY3nnk3FkV5kmGtdKsodkhbQ')
+auth.set_access_token('1131180954851131392-3mM1DFFs1CRr80YaBMsMrMXSrw5hC9', 's9STIOmxPZHXyDo6I3GYUorJ29LDYa2SGpPru6Os9MGtc')
+
+api = tweepy.API(auth)
+
+csvFile = open('data/tweets.csv', 'a')
+csvWriter = csv.writer(csvFile)
+
+search_results = api.search(q ="glass bottle",count = 100)
+
+
+#for tweet in search_results:
+#    tweet.text = tweet.text[2:]
+
+for tweet in search_results:
+    print(tweet.text)
+    print('\n')
+    csvWriter.writerow([tweet.created_at, tweet.text.encode('utf-8')])
+
+
+csvFile.close()
